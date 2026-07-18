1f9107d859b471f9eb60f788a842659b92dea139
EddieFontenele <eqfontenele@gmail.com>
2026-07-18 15:42:36 -0300

    Corrige configuração das fontes

--- app/fonts.ts
+++ app/fonts.ts
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 export const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
   weight: "400",
+  variable: "--font-instrument-serif",
 });
 
 export const cheyra = localFont({
